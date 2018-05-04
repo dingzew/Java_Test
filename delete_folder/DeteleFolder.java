a7c4786d78b69ba99206f0b8a05f5dd02cc5fc57
Dingze Wang <dingzew@Dingzes-MacBook-Pro.local>
2018-05-04 01:31:47 -0400

    delete folder with java

---
+++ delete_folder/DeteleFolder.java
@@ -0,0 +1,25 @@
+package edu.cmu.cs.cs214.hw5.framework;
+
+import java.io.File;
+
+public class DeteleFolder {
+    public static void main(String[] args) {
+        File file =  new File("/Users/dingzew/Documents/testDir");
+        System.out.println(deleteDirectory(file));
+    }
+
+
+    public static boolean deleteDirectory(File dir) {
+        if (dir.isDirectory()) {
+            File[] children = dir.listFiles();
+            for (int i = 0; i < children.length; i++) {
+                boolean success = deleteDirectory(children[i]);
+                if (!success) {
+                    return false;
+                }
+            }
+        } // either file or an empty directory System.out.println("removing file or directory : " + dir.getName()); return dir.delete(); }
+        System.out.println("removing file or directory : " + dir.getName());
+        return dir.delete();
+    }
+}
